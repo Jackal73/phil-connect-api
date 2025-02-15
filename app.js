76a250c448205707c9c3aa6f2321a1a4751659fc
Shawn Kebel <fxrender@outlook.com>
2025-02-15 13:36:20 -0600

    :necktie: Update server port configuration and clean up CORS origin comments

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const corsOptions = {
     "https://phil-connect.adaptable.app", // access-control-allow-origin: http://phil-connect.adaptable.app,
     "https://localhost:3001",
     "https://localhost:3002",
-    "https://phil-connect-api.onrender.com", //  access-control-allow-origin: https://phil-connect-api.onrender.com,
+    "https://phil-connect-api.onrender.com", // access-control-allow-origin: https://phil-connect-api.onrender.com,
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true, // access-control-allow-credentials:true,
@@ -22,7 +22,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 
 // -------------- PORT to use ---------------------------
-const port = process.env.PORT || 8800;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
